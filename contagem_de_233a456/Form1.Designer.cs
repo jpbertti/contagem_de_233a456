154aad2dd4af12290eab49f185f902f8f5a2c3a8
jpbertti <joaobertty@gmail.com>
2024-11-18 11:52:48 -0300

    Adicionar arquivos de projeto.

---
+++ contagem_de_233a456/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace contagem_de_233a456
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lsbResultado1 = new System.Windows.Forms.ListBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsbResultado2 = new System.Windows.Forms.ListBox();
+            this.btnMostrarR2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lsbResultado3 = new System.Windows.Forms.ListBox();
+            this.btnMostrarR3 = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(153, 283);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(88, 37);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(57, 197);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(88, 37);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lsbResultado1
+            // 
+            this.lsbResultado1.FormattingEnabled = true;
+            this.lsbResultado1.Location = new System.Drawing.Point(46, 44);
+            this.lsbResultado1.Name = "lsbResultado1";
+            this.lsbResultado1.Size = new System.Drawing.Size(121, 147);
+            this.lsbResultado1.TabIndex = 2;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(10, 12);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(201, 20);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Contagem de 233 a 456";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "300 a 400 de 3 em 3";
+            // 
+            // lsbResultado2
+            // 
+            this.lsbResultado2.Cursor = System.Windows.Forms.Cursors.No;
+            this.lsbResultado2.FormattingEnabled = true;
+            this.lsbResultado2.Location = new System.Drawing.Point(249, 44);
+            this.lsbResultado2.Name = "lsbResultado2";
+            this.lsbResultado2.Size = new System.Drawing.Size(121, 147);
+            this.lsbResultado2.TabIndex = 5;
+            // 
+            // btnMostrarR2
+            // 
+            this.btnMostrarR2.Cursor = System.Windows.Forms.Cursors.No;
+            this.btnMostrarR2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarR2.Location = new System.Drawing.Point(260, 197);
+            this.btnMostrarR2.Name = "btnMostrarR2";
+            this.btnMostrarR2.Size = new System.Drawing.Size(88, 37);
+            this.btnMostrarR2.TabIndex = 4;
+            this.btnMostrarR2.Text = "Mostrar";
+            this.btnMostrarR2.UseVisualStyleBackColor = true;
+            this.btnMostrarR2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(413, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 40);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "233 a 456 de 5 em 5 exlcuindo \r\no intervalo de 300 a 400";
+            // 
+            // lsbResultado3
+            // 
+            this.lsbResultado3.Cursor = System.Windows.Forms.Cursors.No;
+            this.lsbResultado3.FormattingEnabled = true;
+            this.lsbResultado3.Location = new System.Drawing.Point(427, 44);
+            this.lsbResultado3.Name = "lsbResultado3";
+            this.lsbResultado3.Size = new System.Drawing.Size(121, 147);
+            this.lsbResultado3.TabIndex = 8;
+            // 
+            // btnMostrarR3
+            // 
+            this.btnMostrarR3.Cursor = System.Windows.Forms.Cursors.No;
+            this.btnMostrarR3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarR3.Location = new System.Drawing.Point(438, 197);
+            this.btnMostrarR3.Name = "btnMostrarR3";
+            this.btnMostrarR3.Size = new System.Drawing.Size(88, 37);
+            this.btnMostrarR3.TabIndex = 7;
+            this.btnMostrarR3.Text = "Mostrar";
+            this.btnMostrarR3.UseVisualStyleBackColor = true;
+            this.btnMostrarR3.Click += new System.EventHandler(this.btnMostrarR3_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(355, 283);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(88, 37);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 342);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lsbResultado3);
+            this.Controls.Add(this.btnMostrarR3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lsbResultado2);
+            this.Controls.Add(this.btnMostrarR2);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lsbResultado1);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lsbResultado1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lsbResultado2;
+        private System.Windows.Forms.Button btnMostrarR2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lsbResultado3;
+        private System.Windows.Forms.Button btnMostrarR3;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ contagem_de_233a456/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contagem_de_233a456
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lsbResultado1.Items.Clear();
+            for (int i = 233; i <= 456; i++)
+            {
+                lsbResultado1.Items.Add(i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lsbResultado2.Items.Clear();
+            for (int i = 300; i <= 400; i += 3)
+            {
+                lsbResultado2.Items.Add(i);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lsbResultado1.Items.Clear();
+            lsbResultado2.Items.Clear();
+            lsbResultado3.Items.Clear();
+        }
+
+        private void btnMostrarR3_Click(object sender, EventArgs e)
+        {
+            for (int i = 233; i <=456; i += 5)
+            {
+                if (i>= 300 && i<= 400)
+                {
+                    continue;
+                }
+
+                lsbResultado3.Items.Add(i);
+            }
+        }
+    }
+}
